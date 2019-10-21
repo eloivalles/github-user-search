611c2203ab98d01c95465a737588c8aa962c2f03
DESKTOP-Q3U30H5\Eloi <eloo66@gmail.com>
2019-10-21 11:30:38 +0200

    Auto-generated commit

--- src/app/core/pipes/safeurl.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SafeurlPipe } from './safeurl';
-
-describe('SafeurlPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SafeurlPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
